ce0eb624a54bc3a33ec91f25fc18ac86ac5351c2
Ali Emre Ok <aemreok@gmail.com>
2023-01-16 14:58:54 +0300

    Code Generator developments

---
+++ CodeGenerator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = GenerateCode();
+
+            Console.WriteLine("Code => " + code);
+            WriteToConsoleCodeIsValid(code);
+            WriteToConsoleCodeIsValid("IJKLM6AB");
+            WriteToConsoleCodeIsValid("234");
+        }
+
+        public static bool IsValidCode(string code)
+        {
+            string charachterSet = "ACDEFGHKLMNPRTXYZ234579";
+
+            if (code.Length != 8)
+            {
+                return false;
+            }
+
+            return code.All(x => charachterSet.Contains(x));
+        }
+
+        public static string GenerateCode()
+        {
+            string charachterSet = "ACDEFGHKLMNPRTXYZ234579";
+            var random = new Random();
+
+            List<string> charachterSetList = new List<string>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                charachterSetList.Add(charachterSet);
+            }
+
+            var charArray = charachterSetList.Select(x => x[random.Next(charachterSet.Length)]).ToArray();
+
+            return new string(charArray);
+        }
+
+        public static void WriteToConsoleCodeIsValid(string code)
+        {
+            if (IsValidCode(code))
+            {
+                Console.WriteLine(code + " => is valid");
+            }
+            else
+            {
+                Console.WriteLine(code + " => is not valid");
+            }
+        }
+    }
+}
